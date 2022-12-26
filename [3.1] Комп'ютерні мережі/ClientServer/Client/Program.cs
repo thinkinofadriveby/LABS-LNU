e8e71bf693b0a3af407f0d35a1267dfbe5a44eef
thinkinofadriveby <113356174+thinkinofadriveby@users.noreply.github.com>
2022-12-26 12:29:32 +0200

    Add files via upload

---
+++ [3.1] Комп'ютерні мережі/ClientServer/Client/Program.cs
@@ -0,0 +1,80 @@
+﻿//  Client
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    SendMessageFromClient(11000);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+            }
+        }
+
+        static void SendMessageFromClient(int port)
+        {
+            byte[] bytes = new byte[1024];
+
+            // З'єднуємось з віддаленим пристроєм
+
+            // Встановлюємо віддалену точку для сокета
+            IPHostEntry ipHost = Dns.GetHostEntry("localhost");
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, port);
+
+            Socket sender = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            // З'єднуємо сокет з віддаленою точкою
+            sender.Connect(ipEndPoint);
+
+            string correctLogin = "Nazar Yuras";
+            string correctPass = "123456";
+
+            Console.Write("Input login: ");
+            string login = Console.ReadLine();
+
+            Console.Write("Input password: ");
+            string pass = Console.ReadLine();
+
+            //Console.WriteLine("\nConnecting with {0} ", sender.RemoteEndPoint.ToString());
+            byte[] log = Encoding.UTF8.GetBytes(login);
+            byte[] password = Encoding.UTF8.GetBytes(pass);
+
+            // Відправляємо дані через сокет
+            if (login == correctLogin && pass == correctPass)
+            {
+                int loginSent = sender.Send(log);
+                int passSent = sender.Send(password);
+                Console.WriteLine($"\n[!] Server: Hello, {login}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input!");
+            }
+
+            // Отримуємо відповідь від сервера
+            int bytesRec = sender.Receive(bytes);
+
+            Console.WriteLine("[!] Server: {0}\n\n", Encoding.UTF8.GetString(bytes, 0, bytesRec));
+
+            if (login != correctLogin || pass != correctPass)
+                SendMessageFromClient(port);
+
+            // Звільнюємо сокет
+            sender.Shutdown(SocketShutdown.Both);
+            sender.Close();
+        }
+    }
+}
---
+++ [3.1] Комп'ютерні мережі/ClientServer/ClientServer/Program.cs
@@ -0,0 +1,69 @@
+﻿// Server
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server  
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Встановлюємо для сокета локальну кінцеву точку
+            IPHostEntry ipHost = Dns.GetHostEntry("localhost");
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, 11000);
+
+            // Створюємо сокет TСР/ІР
+            Socket sListener = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            // Призначаємо сокет локальній кінцевій точці і "слухаємо" вхідні сокети
+            try
+            {
+                sListener.Bind(ipEndPoint);
+                sListener.Listen(10);
+
+                // Починаємо "слухати" з'єднання
+                while (true)
+                {
+                    Console.WriteLine("[!] Expecting a connection through the port {0}", ipEndPoint);
+
+                    // Програма призупиняється, очікуючи вхідне з'єднання
+                    Socket handler = sListener.Accept();
+                    string login = null;
+                    string password = null;
+
+                    // Дочікуємось клієнта, який намагається з нами з'єднатись
+
+                    byte[] log = new byte[1024];
+                    int bytesLog = handler.Receive(log);
+                    byte[] pass = new byte[1024];
+                    int bytesPass = handler.Receive(pass);
+
+                    login += Encoding.UTF8.GetString(log, 0, bytesLog);
+                    password += Encoding.UTF8.GetString(pass, 0, bytesPass);
+
+                    // Виводимо його повідомлення в консоль
+                    Console.Write("\nReceived message: LOGIN: " + login + ", PASSWORD: " + password + "\n\n");
+
+                    // Відправляємо відповідь клієнту від сервера
+                    string reply = "Request processed successfully";
+                    byte[] msg = Encoding.UTF8.GetBytes(reply);
+                    handler.Send(msg);
+
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
